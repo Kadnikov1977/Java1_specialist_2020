1dcf3e22bcfaffe11b3696cdccd46e2bf968490f
Kadnikov1977 <43623@mail.ru>
2022-10-18 12:53:47 +0400

    add constructor

--- Point/src/Point2D.java
+++ Point/src/Point2D.java
@@ -3,19 +3,19 @@ public class Point2D {
     private int x, y;
 
     public Point2D() {
-        this(0, 0);  // это конструктор умолчаний, а конструктор по умолчанию создается Java когда вообще нет конструкторов, замыкаем на основной коструктор
+        this(0, 0);  // это конструктор умолчаний, а конструктор по умолчанию создается Java когда вообще нет конструкторов, передаем управление на основной полный коструктор
     }
 
     public Point2D(int x, int y) {
-        this.x = x;
-        this.y = y;
+        setX(x); // тут лучше передавать на сеттеры, т кт там может быть логика
+        setY(y);
     }
 
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
+    public final void setX(int x) { // лучше сделать сеттеры final, чтобы из нельзя было переопределить
         this.x = x;
     }
 
@@ -23,7 +23,7 @@ public class Point2D {
         return y;
     }
 
-    public void setY(int y) {
+    public final void setY(int y) { // лучше сделать сеттеры final, чтобы из нельзя было переопределить
         this.y = y;
     }
 
