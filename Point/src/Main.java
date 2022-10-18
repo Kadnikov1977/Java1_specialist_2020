531935dadb61aa0151d998eef664d128325c5a44
Kadnikov1977 <43623@mail.ru>
2022-10-18 12:44:51 +0400

    add constructor

--- Point/src/Main.java
+++ Point/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        Point2D p1 = new Point2D();
+        p1.setX(-1);
+        p1.setY(2);
+        System.out.println(p1);
+
+        Point2D p2 = new Point2D(2, 4);
+        System.out.println(p2);
+    }
 }
